1ec0430e2c816974c0a9d236bba956181f0e0852
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-10-14 15:47:07 +0530

    notion page link

--- play-with-DSA/src/main/java/com/example/ArrayList_vs_LinkedList.java
+++ play-with-DSA/src/main/java/com/example/ArrayList_vs_LinkedList.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ArrayList_vs_LinkedList {
+package com.example;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayList_vs_LinkedList {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>(100000);
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        compare(arrayList);
+        compare(linkedList);
+
+    }
+
+    private static void compare(List<String> list) {
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list.add("Element " + i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(list.getClass().getName() + " -> Time taken: " + (end - start) + "ms");
+
+    }
 }
